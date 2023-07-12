3a8c27bbd8b3bb85e26c2f3f0c2c34f69325b0f4
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-13 00:21:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function decimalToBinary(num) {
   // Write your code here
+	let ans = "";
+	while(num!=0){
+		ans = ans + num%2;
+		num = num/2;
+	}
+	console.log(ans);
 }
 
 window.decimalToBinary = decimalToBinary;
